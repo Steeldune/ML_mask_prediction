d811f1da258a238df21e65d5eb1570374a9ec261
Simon Steeldune <nighttagle@gmail.com>
2021-05-17 09:56:36 +0200

    Getting progress on the Qu implementation

--- mask-prediction/apply_weights.py
+++ mask-prediction/apply_weights.py
@@ -18,14 +18,15 @@ def blanket_weights(mask, c, img_size=1024):
     new_mask = np.dstack([mask, new_mask_1, new_mask_2])
     return new_mask
 
+
 def get_cluster_center(in_list):
     if len(in_list) == 1:
         return in_list[0]
     pos_sum = np.sum(in_list, axis=0)
     return pos_sum / (np.shape(in_list)[0])
 
-def get_radius_sample(mask_directory):
 
+def get_radius_sample(mask_directory):
     img_list = glob(mask_directory + '*.png')
     diameters = []
     for img in img_list:
@@ -34,7 +35,7 @@ def get_radius_sample(mask_directory):
         bounds = [cv2.boundingRect(cnt) for cnt in contours]
         for bound in bounds:
             if bound[3] + bound[2] >= 20:
-                diameters.append((bound[3] + bound[2])//2)
+                diameters.append((bound[3] + bound[2]) // 2)
 
     return np.array(diameters)
 
@@ -55,24 +56,29 @@ def get_annotated_control_points(image):
 
 
 def add_circles(ctrl_points, diameter):
-    ret_img = np.ones((1024, 1024), dtype=int)*255
+    ret_img = np.zeros((1024, 1024), dtype=int)
     for pnt in ctrl_points:
-        cv2.circle(ret_img, pnt, diameter//2, 0, thickness=-1)
+        cv2.circle(ret_img, pnt, diameter, 255, thickness=-1)
     return ret_img
+
 
 def add_gauss(image, ctrl_points, diameter):
     x = np.linspace(0, 1023, 1024, dtype=int)
-    xv, yv = np.meshgrid(x,x)
+    xv, yv = np.meshgrid(x, x)
     out_image = np.zeros((1024, 1024))
     for pnt in ctrl_points:
         xv_dist = np.abs(xv - pnt[0])
         yv_dist = np.abs(yv - pnt[1])
         dist_map = (np.power(xv_dist, 2) + np.power(yv_dist, 2))
+        dist_map_thresh = dist_map < np.power(diameter // 2, 2)
 
-        gauss_map = np.exp((dist_map*-1)/diameter)*255
-        out_image = out_image + gauss_map
+        gauss_map = np.exp((dist_map * -1) / (diameter * 2)) * 255
+
+        out_image = out_image + gauss_map * dist_map_thresh
 
     return out_image
+
+
 if __name__ == '__main__':
     # mask_list = glob('X:\\BEP_data\\Train_set\\Train_masks\\1\\*.png')
     # for img in mask_list:
@@ -86,6 +92,6 @@ if __name__ == '__main__':
     example_img_2 = add_circles(ctrl_list, 98)
     gauss_img = add_gauss(example_img, ctrl_list, 98)
     final_img = np.dstack([example_img_2, gauss_img, np.zeros((1024, 1024), dtype=int)])
-    cv2.imshow('final', final_img/255)
+    cv2.imshow('final', final_img / 255)
     cv2.waitKey()
     cv2.destroyAllWindows()
